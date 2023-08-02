83ab3a5adcbf05ed69ef95aab229d148932af4ac
nkstlrv <stlrvnk@gmail.com>
2023-08-02 15:16:17 +0300

    app running inside module

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 app = FastAPI()
@@ -6,3 +7,7 @@ app = FastAPI()
 @app.get('/')
 def index():
     return {'message': 'Hello, there!'}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000, reload=True)
